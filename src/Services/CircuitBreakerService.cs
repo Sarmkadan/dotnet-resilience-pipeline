d165d19dc6d6fd4659a5ed2d66654501183e6ba8
Vladyslav Zaiets <rutova2@gmail.com>
2021-08-11 16:31:00 +0300

    refactor(controllers): polish MetricsController

---
+++ src/Services/CircuitBreakerService.cs
@@ -0,0 +1,93 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Diagnostics;
+using DotNetResiliencePipeline.Domain.Policies;
+using DotNetResiliencePipeline.Exceptions;
+
+namespace DotNetResiliencePipeline.Services;
+
+/// <summary>
+/// Service handling circuit breaker policy execution and state management.
+/// </summary>
+public class CircuitBreakerService
+{
+    /// <summary>
+    /// Executes an operation through the circuit breaker policy.
+    /// </summary>
+    public async Task<T> ExecuteAsync<T>(
+        CircuitBreakerPolicy policy,
+        Func<Task<T>> operation)
+    {
+        if (policy == null)
+            throw new ArgumentNullException(nameof(policy));
+
+        if (!policy.IsEnabled)
+            return await operation();
+
+        // Check if circuit should transition to half-open
+        policy.AttemptReset();
+
+        var currentState = policy.CurrentState;
+
+        if (currentState == CircuitBreakerPolicy.CircuitState.Open)
+        {
+            throw new CircuitBreakerOpenException(
+                policy.Name,
+                policy.TimeUntilHalfOpen ?? TimeSpan.Zero,
+                policy.ConsecutiveFailures);
+        }
+
+        var stopwatch = Stopwatch.StartNew();
+
+        try
+        {
+            var result = await operation();
+            stopwatch.Stop();
+
+            policy.RecordSuccess();
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            stopwatch.Stop();
+            policy.RecordFailure();
+
+            throw;
+        }
+    }
+
+    /// <summary>
+    /// Manually opens a circuit breaker.
+    /// </summary>
+    public void OpenCircuit(CircuitBreakerPolicy policy)
+    {
+        if (policy == null)
+            throw new ArgumentNullException(nameof(policy));
+
+        // Simulate opening by setting high failure count
+        policy.RecordFailure();
+    }
+
+    /// <summary>
+    /// Manually resets a circuit breaker.
+    /// </summary>
+    public void ResetCircuit(CircuitBreakerPolicy policy)
+    {
+        if (policy == null)
+            throw new ArgumentNullException(nameof(policy));
+
+        policy.ManualReset();
+    }
+
+    /// <summary>
+    /// Gets the current state of a circuit breaker.
+    /// </summary>
+    public string GetCircuitState(CircuitBreakerPolicy policy)
+    {
+        return policy?.CurrentState.ToString() ?? "Unknown";
+    }
+}
---
+++ src/Utilities/PerformanceMonitor.cs
@@ -0,0 +1,212 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Diagnostics;
+
+namespace DotNetResiliencePipeline.Utilities;
+
+/// <summary>
+/// Monitors performance metrics for policy executions.
+/// Tracks execution times, throughput, and identifies performance degradation.
+/// </summary>
+public class PerformanceMonitor
+{
+    private readonly Dictionary<string, PerformanceMetrics> _metrics = new();
+    private readonly object _lockObj = new object();
+
+    /// <summary>
+    /// Records an execution with its duration.
+    /// </summary>
+    public void RecordExecution(string policyName, long durationMs, bool success)
+    {
+        lock (_lockObj)
+        {
+            if (!_metrics.ContainsKey(policyName))
+                _metrics[policyName] = new PerformanceMetrics { PolicyName = policyName };
+
+            var metrics = _metrics[policyName];
+            metrics.TotalExecutions++;
+            metrics.TotalDurationMs += durationMs;
+
+            if (success)
+                metrics.SuccessfulExecutions++;
+            else
+                metrics.FailedExecutions++;
+
+            // Track percentiles
+            metrics.AllDurations.Add(durationMs);
+            if (metrics.AllDurations.Count > 1000)
+                metrics.AllDurations.RemoveAt(0); // Keep bounded
+        }
+    }
+
+    /// <summary>
+    /// Gets performance metrics for a policy.
+    /// </summary>
+    public PerformanceMetrics GetMetrics(string policyName)
+    {
+        lock (_lockObj)
+        {
+            if (_metrics.TryGetValue(policyName, out var metrics))
+                return new PerformanceMetrics
+                {
+                    PolicyName = metrics.PolicyName,
+                    TotalExecutions = metrics.TotalExecutions,
+                    TotalDurationMs = metrics.TotalDurationMs,
+                    SuccessfulExecutions = metrics.SuccessfulExecutions,
+                    FailedExecutions = metrics.FailedExecutions
+                };
+
+            return new PerformanceMetrics { PolicyName = policyName };
+        }
+    }
+
+    /// <summary>
+    /// Gets all performance metrics.
+    /// </summary>
+    public List<PerformanceMetrics> GetAllMetrics()
+    {
+        lock (_lockObj)
+        {
+            return _metrics.Values.ToList();
+        }
+    }
+
+    /// <summary>
+    /// Identifies slow-running policies.
+    /// </summary>
+    public List<PerformanceIssue> IdentifyPerformanceIssues(long slowThresholdMs = 1000)
+    {
+        lock (_lockObj)
+        {
+            var issues = new List<PerformanceIssue>();
+
+            foreach (var metrics in _metrics.Values)
+            {
+                if (metrics.AverageDurationMs > slowThresholdMs)
+                {
+                    issues.Add(new PerformanceIssue
+                    {
+                        PolicyName = metrics.PolicyName,
+                        IssueType = "SlowExecution",
+                        AverageDurationMs = metrics.AverageDurationMs,
+                        Severity = metrics.AverageDurationMs > slowThresholdMs * 5 ? "Critical" : "Warning"
+                    });
+                }
+
+                if (metrics.FailureRate > 0.5) // More than 50% failure
+                {
+                    issues.Add(new PerformanceIssue
+                    {
+                        PolicyName = metrics.PolicyName,
+                        IssueType = "HighFailureRate",
+                        FailureRate = metrics.FailureRate,
+                        Severity = "Warning"
+                    });
+                }
+            }
+
+            return issues;
+        }
+    }
+
+    /// <summary>
+    /// Clears all performance metrics.
+    /// </summary>
+    public void Clear()
+    {
+        lock (_lockObj)
+        {
+            _metrics.Clear();
+        }
+    }
+
+    /// <summary>
+    /// Gets comparative performance metrics between policies.
+    /// </summary>
+    public List<PerformanceComparison> ComparePerformance()
+    {
+        lock (_lockObj)
+        {
+            if (_metrics.Count == 0)
+                return new();
+
+            var slowest = _metrics.Values.OrderByDescending(m => m.AverageDurationMs).First();
+            var fastest = _metrics.Values.OrderByDescending(m => m.AverageDurationMs).Last();
+
+            return _metrics.Values.Select(m => new PerformanceComparison
+            {
+                PolicyName = m.PolicyName,
+                AverageDurationMs = m.AverageDurationMs,
+                PercentageOfSlowest = (m.AverageDurationMs * 100.0) / slowest.AverageDurationMs,
+                SuccessRate = m.SuccessRate
+            }).OrderByDescending(c => c.AverageDurationMs).ToList();
+        }
+    }
+}
+
+/// <summary>
+/// Performance metrics for a policy.
+/// </summary>
+public class PerformanceMetrics
+{
+    public string PolicyName { get; set; } = string.Empty;
+    public long TotalExecutions { get; set; }
+    public long TotalDurationMs { get; set; }
+    public long SuccessfulExecutions { get; set; }
+    public long FailedExecutions { get; set; }
+
+    public double AverageDurationMs =>
+        TotalExecutions > 0 ? (double)TotalDurationMs / TotalExecutions : 0;
+
+    public double SuccessRate =>
+        TotalExecutions > 0 ? (SuccessfulExecutions * 100.0) / TotalExecutions : 0;
+
+    public double FailureRate =>
+        TotalExecutions > 0 ? (FailedExecutions * 100.0) / TotalExecutions : 0;
+
+    public double ThroughputPerSecond =>
+        TotalDurationMs > 0 ? (TotalExecutions * 1000.0) / TotalDurationMs : 0;
+
+    public List<long> AllDurations { get; set; } = new();
+
+    public long P50 => CalculatePercentile(50);
+    public long P90 => CalculatePercentile(90);
+    public long P95 => CalculatePercentile(95);
+    public long P99 => CalculatePercentile(99);
+
+    private long CalculatePercentile(int percentile)
+    {
+        if (AllDurations.Count == 0)
+            return 0;
+
+        var sorted = AllDurations.OrderBy(x => x).ToList();
+        int index = (int)((percentile / 100.0) * sorted.Count);
+        return sorted[Math.Min(index, sorted.Count - 1)];
+    }
+}
+
+/// <summary>
+/// Performance issue identified by the monitor.
+/// </summary>
+public class PerformanceIssue
+{
+    public string PolicyName { get; set; } = string.Empty;
+    public string IssueType { get; set; } = string.Empty;
+    public double AverageDurationMs { get; set; }
+    public double FailureRate { get; set; }
+    public string Severity { get; set; } = string.Empty;
+}
+
+/// <summary>
+/// Comparative performance metrics between policies.
+/// </summary>
+public class PerformanceComparison
+{
+    public string PolicyName { get; set; } = string.Empty;
+    public double AverageDurationMs { get; set; }
+    public double PercentageOfSlowest { get; set; }
+    public double SuccessRate { get; set; }
+}
