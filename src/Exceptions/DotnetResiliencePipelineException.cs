4809f08688bf7a993c594fe5b68b37c629280037
Vladyslav Zaiets <rutova2@gmail.com>
2021-09-17 07:27:47 +0300

    refactor(exceptions): dedupe some logic in DotnetResiliencePipelineExcep

--- src/Exceptions/DotnetResiliencePipelineException.cs
+++ src/Exceptions/DotnetResiliencePipelineException.cs
@@ -1,9 +1,21 @@
+/// <summary>
+/// Represents an exception that occurs within the Dotnet Resilience Pipeline.
+/// </summary>
 public abstract class DotnetResiliencePipelineException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DotnetResiliencePipelineException"/> class with the specified message.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
     public DotnetResiliencePipelineException(string? message) : base(message)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DotnetResiliencePipelineException"/> class with the specified message and inner exception.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
     public DotnetResiliencePipelineException(string? message, Exception? innerException) : base(message, innerException)
     {
     }
