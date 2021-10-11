fe18c854242206d3124b295a263193067da2dacf
Vladyslav Zaiets <rutova2@gmail.com>
2021-10-11 15:39:30 +0300

    feat: json round-trip support for TimeoutBenchmarksJson

---
+++ benchmarks/TimeoutBenchmarksJsonExtensions.cs
@@ -0,0 +1,67 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DotNetResiliencePipeline.Benchmarks;
+
+public static class TimeoutBenchmarksJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="TimeoutBenchmarks"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The value to serialize.</param>
+    /// <param name="indented">Whether to indent the JSON for readability.</param>
+    /// <returns>A JSON string representation of the value.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
+    public static string ToJson(this TimeoutBenchmarks value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        return JsonSerializer.Serialize(value, indented ? _jsonOptions with { WriteIndented = true } : _jsonOptions);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="TimeoutBenchmarks"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized <see cref="TimeoutBenchmarks"/> instance, or <see langword="null"/> if the JSON is empty.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static TimeoutBenchmarks? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        return json.Length == 0
+            ? null
+            : JsonSerializer.Deserialize<TimeoutBenchmarks>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="TimeoutBenchmarks"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized value if successful.</param>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
+    public static bool TryFromJson(string json, out TimeoutBenchmarks? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<TimeoutBenchmarks>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
