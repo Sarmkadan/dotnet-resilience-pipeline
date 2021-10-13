4f80ddcd3bc85797de4efd370ddd4a646a84972f
Vladyslav Zaiets <rutova2@gmail.com>
2021-10-13 19:01:28 +0300

    json round-trip support for BulkheadPolicyJson
    found this while looking at something else.

---
+++ src/Domain/Policies/BulkheadPolicyJsonExtensions.cs
@@ -0,0 +1,88 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =====================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
+
+namespace DotNetResiliencePipeline.Domain.Policies;
+
+/// <summary>
+/// Provides System.Text.Json serialization extensions for <see cref="BulkheadPolicy"/>.
+/// </summary>
+public static class BulkheadPolicyJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        TypeInfoResolver = new DefaultJsonTypeInfoResolver(),
+        ReferenceHandler = ReferenceHandler.IgnoreCycles
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="BulkheadPolicy"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The bulkhead policy to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the bulkhead policy.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this BulkheadPolicy value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions)
+            {
+                WriteIndented = true
+            }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="BulkheadPolicy"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized bulkhead policy, or null if the JSON is empty.</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static BulkheadPolicy? FromJson(string json)
+    {
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<BulkheadPolicy>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="BulkheadPolicy"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized bulkhead policy if successful.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    public static bool TryFromJson(string json, out BulkheadPolicy? value)
+    {
+        value = null;
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return false;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<BulkheadPolicy>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
