be313876975a952b3d594568ba95feb05d3baab5
Vladyslav Zaiets <rutova2@gmail.com>
2021-10-25 16:36:20 +0300

    feat: json serialization for CircuitBreakerServiceJson

---
+++ src/Services/CircuitBreakerServiceJsonExtensions.cs
@@ -0,0 +1,93 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using DotNetResiliencePipeline.Domain.Policies;
+using DotNetResiliencePipeline.Exceptions;
+
+namespace DotNetResiliencePipeline.Services;
+
+/// <summary>
+/// Provides System.Text.Json serialization and deserialization extensions for <see cref="CircuitBreakerService"/>.
+/// </summary>
+public static class CircuitBreakerServiceJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+    };
+
+    /// <summary>
+    /// Serializes the <see cref="CircuitBreakerService"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The service instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representing the service instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this CircuitBreakerService value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        return JsonSerializer.Serialize(value, _jsonOptions.WithIndented(indented));
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="CircuitBreakerService"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized service instance, or null if the JSON is empty or whitespace.</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static CircuitBreakerService? FromJson(string json)
+    {
+        if (string.IsNullOrWhiteSpace(json))
+            return null;
+
+        return JsonSerializer.Deserialize<CircuitBreakerService>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="CircuitBreakerService"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized service instance if successful.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    public static bool TryFromJson(string json, out CircuitBreakerService? value)
+    {
+        value = null;
+
+        if (string.IsNullOrWhiteSpace(json))
+            return false;
+
+        try
+        {
+            value = JsonSerializer.Deserialize<CircuitBreakerService>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="JsonSerializerOptions"/> with the specified indentation setting.
+    /// </summary>
+    private static JsonSerializerOptions WithIndented(this JsonSerializerOptions options, bool indented)
+    {
+        if (options.WriteIndented == indented)
+            return options;
+
+        var newOptions = new JsonSerializerOptions(options)
+        {
+            WriteIndented = indented
+        };
+        return newOptions;
+    }
+}
