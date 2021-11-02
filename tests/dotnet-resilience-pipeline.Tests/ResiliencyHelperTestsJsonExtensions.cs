6597ebc9d6538047ca5d258efee60b7e2a82cc85
Vladyslav Zaiets <rutova2@gmail.com>
2021-11-02 11:39:34 +0200

    json serialization for ResiliencyHelperTestsJson

---
+++ tests/dotnet-resilience-pipeline.Tests/ResiliencyHelperTestsJsonExtensions.cs
@@ -0,0 +1,77 @@
+#nullable enable
+
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace DotNetResiliencePipeline.Tests;
+
+/// <summary>
+/// Provides System.Text.Json serialization extensions for <see cref="ResiliencyHelperTests"/>.
+/// </summary>
+public static class ResiliencyHelperTestsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        TypeInfoResolver = new DefaultJsonTypeInfoResolver()
+    };
+
+    /// <summary>
+    /// Serializes the <see cref="ResiliencyHelperTests"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation.</param>
+    /// <returns>A JSON string representation of the instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
+    public static string ToJson(this ResiliencyHelperTests value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="ResiliencyHelperTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized instance, or <see langword="null"/> if the JSON is empty or whitespace.</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static ResiliencyHelperTests? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<ResiliencyHelperTests>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="ResiliencyHelperTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized instance if successful.</param>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    public static bool TryFromJson(string json, out ResiliencyHelperTests? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<ResiliencyHelperTests>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
