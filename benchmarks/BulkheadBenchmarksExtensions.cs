ceacfac1bcb04718f79a1938a0129025cf8249e0
Vladyslav Zaiets <rutova2@gmail.com>
2021-11-26 12:18:00 +0200

    feat(benchmarks): more BulkheadBenchmarks extension methods

--- benchmarks/BulkheadBenchmarksExtensions.cs
+++ benchmarks/BulkheadBenchmarksExtensions.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Benchmarks
+namespace DotNetResiliencePipeline.Benchmarks
 {
     /// <summary>
     /// Extension methods that add convenient, higher‑level operations for <see cref="BulkheadBenchmarks"/>.
@@ -12,9 +12,12 @@ namespace Benchmarks
         /// Attempts to acquire a bulkhead slot and, if successful, immediately releases it.
         /// Returns <c>true</c> when a slot was acquired.
         /// </summary>
+        /// <param name="benchmarks">The <see cref="BulkheadBenchmarks"/> instance.</param>
+        /// <returns><c>true</c> if a slot was acquired; otherwise, <c>false</c>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="benchmarks"/> is <c>null</c>.</exception>
         public static bool TryAcquireAndRelease(this BulkheadBenchmarks benchmarks)
         {
-            if (benchmarks == null) throw new ArgumentNullException(nameof(benchmarks));
+            ArgumentNullException.ThrowIfNull(benchmarks);
 
             bool acquired = benchmarks.BulkheadPolicy_TryAcquireSlot_Available;
             if (acquired)
@@ -29,9 +32,12 @@ namespace Benchmarks
         /// <summary>
         /// Records a queue‑wait event (using the existing benchmark method) and then returns the current utilization percentage.
         /// </summary>
+        /// <param name="benchmarks">The <see cref="BulkheadBenchmarks"/> instance.</param>
+        /// <returns>The current utilization percentage.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="benchmarks"/> is <c>null</c>.</exception>
         public static double RecordQueueWaitAndGetUtilization(this BulkheadBenchmarks benchmarks)
         {
-            if (benchmarks == null) throw new ArgumentNullException(nameof(benchmarks));
+            ArgumentNullException.ThrowIfNull(benchmarks);
 
             // The underlying benchmark records the wait time internally.
             benchmarks.BulkheadPolicy_RecordQueueWaitTime();
@@ -44,9 +50,12 @@ namespace Benchmarks
         /// Returns a short, human‑readable performance summary that includes utilization,
         /// queued and rejection percentages.
         /// </summary>
+        /// <param name="benchmarks">The <see cref="BulkheadBenchmarks"/> instance.</param>
+        /// <returns>A formatted performance summary string.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="benchmarks"/> is <c>null</c>.</exception>
         public static string GetPerformanceSummary(this BulkheadBenchmarks benchmarks)
         {
-            if (benchmarks == null) throw new ArgumentNullException(nameof(benchmarks));
+            ArgumentNullException.ThrowIfNull(benchmarks);
 
             double utilization = benchmarks.BulkheadPolicy_GetUtilizationPercentage();
             double queued = benchmarks.BulkheadPolicy_GetQueuedPercentage();
@@ -57,13 +66,18 @@ namespace Benchmarks
 
         /// <summary>
         /// Determines whether the bulkhead is considered overloaded based on a utilization threshold.
-        /// The default threshold is 80 % (0.8).
+        /// The default threshold is 80 % (0.8).
         /// </summary>
+        /// <param name="benchmarks">The <see cref="BulkheadBenchmarks"/> instance.</param>
+        /// <param name="utilizationThreshold">The utilization threshold between 0.0 and 1.0. Defaults to 0.8.</param>
+        /// <returns><c>true</c> if the bulkhead utilization exceeds the threshold; otherwise, <c>false</c>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="benchmarks"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="utilizationThreshold"/> is outside the valid range of 0.0 to 1.0.</exception>
         public static bool IsOverloaded(this BulkheadBenchmarks benchmarks, double utilizationThreshold = 0.8)
         {
-            if (benchmarks == null) throw new ArgumentNullException(nameof(benchmarks));
-            if (utilizationThreshold < 0.0 || utilizationThreshold > 1.0)
-                throw new ArgumentOutOfRangeException(nameof(utilizationThreshold), "Threshold must be between 0.0 and 1.0.");
+            ArgumentNullException.ThrowIfNull(benchmarks);
+            ArgumentOutOfRangeException.ThrowIfLessThan(utilizationThreshold, 0.0);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(utilizationThreshold, 1.0);
 
             return benchmarks.BulkheadPolicy_GetUtilizationPercentage() > utilizationThreshold;
         }
