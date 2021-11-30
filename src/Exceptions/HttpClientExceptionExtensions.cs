608970895435eb9b7f8ed46054385b17520dd944
Vladyslav Zaiets <rutova2@gmail.com>
2021-11-30 06:36:00 +0200

    Handy extensions for HttpClientException

--- src/Exceptions/HttpClientExceptionExtensions.cs
+++ src/Exceptions/HttpClientExceptionExtensions.cs
@@ -3,7 +3,7 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =============================================================================
+// =====================================================================
 
 using System;
 using System.Net;
@@ -12,7 +12,7 @@ using System.Text;
 namespace DotNetResiliencePipeline.Exceptions;
 
 /// <summary>
-/// Extension methods for HttpClientException and derived types.
+/// Extension methods for <see cref="HttpClientException"/> and derived types.
 /// </summary>
 public static class HttpClientExceptionExtensions
 {
@@ -20,40 +20,38 @@ public static class HttpClientExceptionExtensions
     /// Gets a formatted error message that includes client name, request URL, and HTTP method (if available).
     /// </summary>
     /// <param name="exception">The exception instance.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <see langword="null"/>.</exception>
     /// <returns>A formatted error message string.</returns>
     public static string GetFullErrorMessage(this HttpClientException exception)
     {
-        if (exception is null)
-        {
-            throw new ArgumentNullException(nameof(exception));
-        }
+        ArgumentNullException.ThrowIfNull(exception);
 
         var messageBuilder = new StringBuilder();
         messageBuilder.Append(exception.Message);
 
         if (!string.IsNullOrEmpty(exception.ClientName))
         {
-            messageBuilder.Append($" | Client: {exception.ClientName}");
+            messageBuilder.Append(" | Client: ").Append(exception.ClientName);
         }
 
         if (!string.IsNullOrEmpty(exception.RequestUrl))
         {
-            messageBuilder.Append($" | URL: {exception.RequestUrl}");
+            messageBuilder.Append(" | URL: ").Append(exception.RequestUrl);
         }
 
         if (exception is InvalidHttpRequestException invalidRequestException && !string.IsNullOrEmpty(invalidRequestException.HttpMethod))
         {
-            messageBuilder.Append($" | Method: {invalidRequestException.HttpMethod}");
+            messageBuilder.Append(" | Method: ").Append(invalidRequestException.HttpMethod);
         }
 
         if (exception is HttpResponseException responseException)
         {
-            messageBuilder.Append($" | Status: {(int)responseException.StatusCode} ({responseException.StatusCode})");
+            messageBuilder.Append(" | Status: ").Append((int)responseException.StatusCode).Append(" (").Append(responseException.StatusCode).Append(')');
         }
 
         if (exception is HttpTimeoutException timeoutException)
         {
-            messageBuilder.Append($" | Timeout: {timeoutException.Timeout.TotalSeconds}s");
+            messageBuilder.Append(" | Timeout: ").Append(timeoutException.Timeout.TotalSeconds).Append('s');
         }
 
         return messageBuilder.ToString();
@@ -63,13 +61,11 @@ public static class HttpClientExceptionExtensions
     /// Determines if the exception represents a client error (4xx status code).
     /// </summary>
     /// <param name="exception">The exception instance.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <see langword="null"/>.</exception>
     /// <returns>True if the exception is a client error; otherwise, false.</returns>
     public static bool IsClientError(this HttpClientException exception)
     {
-        if (exception is null)
-        {
-            throw new ArgumentNullException(nameof(exception));
-        }
+        ArgumentNullException.ThrowIfNull(exception);
 
         return exception is HttpResponseException responseException &&
                responseException.StatusCode >= (int)HttpStatusCode.BadRequest &&
@@ -80,13 +76,11 @@ public static class HttpClientExceptionExtensions
     /// Determines if the exception represents a server error (5xx status code).
     /// </summary>
     /// <param name="exception">The exception instance.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <see langword="null"/>.</exception>
     /// <returns>True if the exception is a server error; otherwise, false.</returns>
     public static bool IsServerError(this HttpClientException exception)
     {
-        if (exception is null)
-        {
-            throw new ArgumentNullException(nameof(exception));
-        }
+        ArgumentNullException.ThrowIfNull(exception);
 
         return exception is HttpResponseException responseException &&
                responseException.StatusCode >= (int)HttpStatusCode.InternalServerError;
@@ -96,9 +90,12 @@ public static class HttpClientExceptionExtensions
     /// Determines if the exception represents a timeout error.
     /// </summary>
     /// <param name="exception">The exception instance.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <see langword="null"/>.</exception>
     /// <returns>True if the exception is a timeout error; otherwise, false.</returns>
     public static bool IsTimeoutError(this HttpClientException exception)
     {
+        ArgumentNullException.ThrowIfNull(exception);
+
         return exception is HttpTimeoutException;
     }
 
@@ -106,18 +103,16 @@ public static class HttpClientExceptionExtensions
     /// Gets a simplified error code for the exception type.
     /// </summary>
     /// <param name="exception">The exception instance.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <see langword="null"/>.</exception>
     /// <returns>A string representing the error code.</returns>
     public static string GetErrorCode(this HttpClientException exception)
     {
-        if (exception is null)
-        {
-            throw new ArgumentNullException(nameof(exception));
-        }
+        ArgumentNullException.ThrowIfNull(exception);
 
         return exception switch
         {
             HttpTimeoutException => "HTTP_TIMEOUT",
-            HttpResponseException => $"HTTP_{exception.GetType().Name.Replace("Http", "").Replace("Exception", "")}",
+            HttpResponseException => $"HTTP_{exception.GetType().Name[4..^9]}",
             InvalidHttpRequestException => "HTTP_INVALID_REQUEST",
             _ => "HTTP_CLIENT_ERROR"
         };
