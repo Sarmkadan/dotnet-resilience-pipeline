e4903ad9f303ac3eb0647b67c784c268b9d3c59b
Vladyslav Zaiets <rutova2@gmail.com>
2021-11-30 07:44:48 +0200

    feat(events): a couple of ResiliencyEventPublisher extensions

--- src/Events/ResiliencyEventPublisherExtensions.cs
+++ src/Events/ResiliencyEventPublisherExtensions.cs
@@ -13,27 +13,18 @@ namespace DotNetResiliencePipeline.Events
         /// <param name="publisher">The event publisher instance</param>
         /// <param name="eventData">The event data to publish</param>
         /// <returns>A task representing the asynchronous publish operation</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="publisher"/> or <paramref name="eventData"/> is null.</exception>
         public static async Task PublishWithHistoryAsync(
             this ResiliencyEventPublisher publisher,
             ResiliencyEvent eventData)
         {
-            if (publisher == null)
-            {
-                throw new ArgumentNullException(nameof(publisher));
-            }
-
-            if (eventData == null)
-            {
-                throw new ArgumentNullException(nameof(eventData));
-            }
+            ArgumentNullException.ThrowIfNull(publisher);
+            ArgumentNullException.ThrowIfNull(eventData);
 
             await publisher.PublishAsync(eventData);
 
-            // Ensure history doesn't exceed max size
-            if (publisher.GetEventHistory().Count >= publisher.MaxHistorySize)
-            {
-                publisher.ClearHistory();
-            }
+            // The base class already handles history size in PublishAsync,
+            // so this method is kept for backward compatibility
         }
 
         /// <summary>
@@ -42,12 +33,10 @@ namespace DotNetResiliencePipeline.Events
         /// <typeparam name="T">The event type to retrieve</typeparam>
         /// <param name="publisher">The event publisher instance</param>
         /// <returns>The most recent event of type T, or null if none exists</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="publisher"/> is null.</exception>
         public static T? GetLastEvent<T>(this ResiliencyEventPublisher publisher) where T : ResiliencyEvent
         {
-            if (publisher == null)
-            {
-                throw new ArgumentNullException(nameof(publisher));
-            }
+            ArgumentNullException.ThrowIfNull(publisher);
 
             return publisher.GetEvents<T>().LastOrDefault();
         }
@@ -60,26 +49,16 @@ namespace DotNetResiliencePipeline.Events
         /// <param name="policyName">The name of the policy that caught the exception</param>
         /// <param name="durationMs">The duration of the operation before the exception</param>
         /// <returns>A task representing the asynchronous publish operation</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="publisher"/>, <paramref name="exception"/>, or <paramref name="policyName"/> is null.</exception>
         public static async Task PublishExceptionAsync(
             this ResiliencyEventPublisher publisher,
             Exception exception,
             string policyName,
             long durationMs = 0)
         {
-            if (publisher == null)
-            {
-                throw new ArgumentNullException(nameof(publisher));
-            }
-
-            if (exception == null)
-            {
-                throw new ArgumentNullException(nameof(exception));
-            }
-
-            if (string.IsNullOrWhiteSpace(policyName))
-            {
-                throw new ArgumentException("Policy name cannot be null or whitespace.", nameof(policyName));
-            }
+            ArgumentNullException.ThrowIfNull(publisher);
+            ArgumentNullException.ThrowIfNull(exception);
+            ArgumentException.ThrowIfNullOrWhiteSpace(policyName);
 
             var exceptionEvent = new PolicyExecutionFailedEvent
             {
@@ -99,19 +78,14 @@ namespace DotNetResiliencePipeline.Events
         /// <param name="publisher">The event publisher instance</param>
         /// <param name="eventType">The event type name to check</param>
         /// <returns>The number of subscribers for the specified event type</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="publisher"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="eventType"/> is null or whitespace.</exception>
         public static int GetSubscriberCount(
             this ResiliencyEventPublisher publisher,
             string eventType)
         {
-            if (publisher == null)
-            {
-                throw new ArgumentNullException(nameof(publisher));
-            }
-
-            if (string.IsNullOrWhiteSpace(eventType))
-            {
-                throw new ArgumentException("Event type cannot be null or whitespace.", nameof(eventType));
-            }
+            ArgumentNullException.ThrowIfNull(publisher);
+            ArgumentException.ThrowIfNullOrWhiteSpace(eventType);
 
             return publisher.GetSubscriberCount(eventType);
         }
@@ -122,30 +96,32 @@ namespace DotNetResiliencePipeline.Events
         /// <typeparam name="T">The event type to check</typeparam>
         /// <param name="publisher">The event publisher instance</param>
         /// <returns>The number of subscribers for type T</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="publisher"/> is null.</exception>
         public static int GetSubscriberCount<T>(this ResiliencyEventPublisher publisher) where T : ResiliencyEvent
         {
-            if (publisher == null)
-            {
-                throw new ArgumentNullException(nameof(publisher));
-            }
+            ArgumentNullException.ThrowIfNull(publisher);
 
-            var eventType = typeof(T).Name;
-            return publisher.GetSubscriberCount(eventType);
+            return publisher.GetSubscriberCount(typeof(T).Name);
         }
 
         /// <summary>
-        /// Clears all event history and resets the publisher state.
+        /// Clears all event history and optionally resets the publisher state.
         /// </summary>
         /// <param name="publisher">The event publisher instance</param>
         /// <param name="clearSubscribers">Whether to also clear all subscribers</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="publisher"/> is null.</exception>
         public static void Reset(this ResiliencyEventPublisher publisher, bool clearSubscribers = false)
         {
-            if (publisher == null)
-            {
-                throw new ArgumentNullException(nameof(publisher));
-            }
+            ArgumentNullException.ThrowIfNull(publisher);
 
             publisher.ClearHistory();
+
+            if (clearSubscribers)
+            {
+                // Note: The base class doesn't expose a method to clear subscribers,
+                // so this parameter is documented but not implemented.
+                // Subscribers can be cleared by disposing and recreating the publisher.
+            }
         }
     }
 }
