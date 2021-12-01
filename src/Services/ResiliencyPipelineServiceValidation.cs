34ef9005fce636fd024d83d98b629c0ad30883a2
Vladyslav Zaiets <rutova2@gmail.com>
2021-12-01 21:16:43 +0200

    ResiliencyPiepline: null/range checks.

--- src/Services/ResiliencyPipelineServiceValidation.cs
+++ src/Services/ResiliencyPipelineServiceValidation.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 
 namespace DotNetResiliencePipeline.Services
 {
+    /// <summary>
+    /// Provides validation methods for <see cref="ResiliencyPipelineService"/> instances.
+    /// </summary>
     public static class ResiliencyPipelineServiceValidation
     {
+        /// <summary>
+        /// Validates a <see cref="ResiliencyPipelineService"/> instance and returns a list of validation problems.
+        /// </summary>
+        /// <param name="value">The service instance to validate.</param>
+        /// <returns>A read-only list of validation error messages. Empty if valid.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
         public static IReadOnlyList<string> Validate(this ResiliencyPipelineService value)
         {
-            var problems = new List<string>();
+            ArgumentNullException.ThrowIfNull(value);
 
-            if (value == null)
-            {
-                problems.Add("ResiliencyPipelineService instance cannot be null.");
-                return problems;
-            }
+            var problems = new List<string>();
 
             if (string.IsNullOrWhiteSpace(value.PipelineId))
             {
@@ -40,14 +45,34 @@ namespace DotNetResiliencePipeline.Services
                 problems.Add("FailedExecutions cannot be negative.");
             }
 
-            return problems;
+            // Validate that successful + failed executions don't exceed total executions
+            if (value.TotalExecutions > 0 && value.SuccessfulExecutions + value.FailedExecutions > value.TotalExecutions)
+            {
+                problems.Add("SuccessfulExecutions + FailedExecutions cannot exceed TotalExecutions.");
+            }
+
+            // Validate that failed executions don't exceed total executions
+            if (value.TotalExecutions > 0 && value.FailedExecutions > value.TotalExecutions)
+            {
+                problems.Add("FailedExecutions cannot exceed TotalExecutions.");
+            }
+
+            return problems.AsReadOnly();
         }
 
-        public static bool IsValid(this ResiliencyPipelineService value)
-        {
-            return value.Validate().Count == 0;
-        }
+        /// <summary>
+        /// Determines whether the specified <see cref="ResiliencyPipelineService"/> is valid.
+        /// </summary>
+        /// <param name="value">The service instance to check.</param>
+        /// <returns><see langword="true"/> if valid; otherwise, <see langword="false"/>.</returns>
+        public static bool IsValid(this ResiliencyPipelineService value) => value.Validate().Count == 0;
 
+        /// <summary>
+        /// Validates a <see cref="ResiliencyPipelineService"/> instance and throws an <see cref="ArgumentException"/> if invalid.
+        /// </summary>
+        /// <param name="value">The service instance to validate.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when validation fails.</exception>
         public static void EnsureValid(this ResiliencyPipelineService value)
         {
             var problems = value.Validate();
