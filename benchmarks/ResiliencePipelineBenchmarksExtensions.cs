0d3fb6195343a84e2cf429558c1342031487dd70
Vladyslav Zaiets <rutova2@gmail.com>
2021-12-03 20:21:42 +0200

    Extension methods on ResiliencePipelineBenchmarks
    found this while looking at something else.

--- benchmarks/ResiliencePipelineBenchmarksExtensions.cs
+++ benchmarks/ResiliencePipelineBenchmarksExtensions.cs
@@ -4,15 +4,17 @@ using System.Threading.Tasks;
 namespace DotNetResiliencePipeline.Benchmarks
 {
     /// <summary>
-    /// Extension methods for <see cref="ResiliencePipelineBenchmarks"/> that provide convenient operations for benchmark scenarios.
+    /// Provides extension methods for <see cref="ResiliencePipelineBenchmarks"/> that simplify common benchmark
+    /// operations such as resetting the pipeline, executing specific benchmark scenarios, and retrieving the
+    /// resulting <see cref="PipelineStatistics"/>.
     /// </summary>
     public static class ResiliencePipelineBenchmarksExtensions
     {
         /// <summary>
-        /// Resets the benchmark pipeline state by invoking the setup routine.
+        /// Resets the benchmark pipeline state by invoking the setup routine on the supplied benchmark instance.
         /// </summary>
-        /// <param name="pipeline">The benchmark instance to reset.</param>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="pipeline"/> is null.</exception>
+        /// <param name="pipeline">The <see cref="ResiliencePipelineBenchmarks"/> instance whose state should be reset.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="pipeline"/> is <c>null</c>.</exception>
         public static void ResetPipelineState(this ResiliencePipelineBenchmarks pipeline)
         {
             ArgumentNullException.ThrowIfNull(pipeline);
@@ -20,11 +22,14 @@ namespace DotNetResiliencePipeline.Benchmarks
         }
 
         /// <summary>
-        /// Executes the successful operation benchmark and collects execution statistics.
+        /// Executes the successful‑operation benchmark scenario and returns the statistics collected during the run.
         /// </summary>
-        /// <param name="pipeline">The benchmark instance.</param>
-        /// <returns>A task that represents the asynchronous operation. The task result contains the pipeline statistics.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="pipeline"/> is null.</exception>
+        /// <param name="pipeline">The <see cref="ResiliencePipelineBenchmarks"/> instance to execute.</param>
+        /// <returns>
+        /// A <see cref="Task{PipelineStatistics}"/> that completes with the <see cref="PipelineStatistics"/>
+        /// gathered from the successful‑operation benchmark.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="pipeline"/> is <c>null</c>.</exception>
         public static async Task<PipelineStatistics> RunAndCollectStatistics(this ResiliencePipelineBenchmarks pipeline)
         {
             ArgumentNullException.ThrowIfNull(pipeline);
@@ -33,11 +38,14 @@ namespace DotNetResiliencePipeline.Benchmarks
         }
 
         /// <summary>
-        /// Executes the full resilience pipeline benchmark and collects execution statistics.
+        /// Executes the full resilience‑pipeline benchmark scenario and returns the statistics collected during the run.
         /// </summary>
-        /// <param name="pipeline">The benchmark instance.</param>
-        /// <returns>A task that represents the asynchronous operation. The task result contains the pipeline statistics.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="pipeline"/> is null.</exception>
+        /// <param name="pipeline">The <see cref="ResiliencePipelineBenchmarks"/> instance to execute.</param>
+        /// <returns>
+        /// A <see cref="Task{PipelineStatistics}"/> that completes with the <see cref="PipelineStatistics"/>
+        /// gathered from the full pipeline benchmark.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="pipeline"/> is <c>null</c>.</exception>
         public static async Task<PipelineStatistics> RunFullPipelineAndCollectStatistics(this ResiliencePipelineBenchmarks pipeline)
         {
             ArgumentNullException.ThrowIfNull(pipeline);
@@ -46,11 +54,14 @@ namespace DotNetResiliencePipeline.Benchmarks
         }
 
         /// <summary>
-        /// Executes multiple parallel operations through the pipeline and collects execution statistics.
+        /// Executes multiple operations in parallel through the benchmark pipeline and returns the collected statistics.
         /// </summary>
-        /// <param name="pipeline">The benchmark instance.</param>
-        /// <returns>A task that represents the asynchronous operation. The task result contains the pipeline statistics.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="pipeline"/> is null.</exception>
+        /// <param name="pipeline">The <see cref="ResiliencePipelineBenchmarks"/> instance to execute.</param>
+        /// <returns>
+        /// A <see cref="Task{PipelineStatistics}"/> that completes with the <see cref="PipelineStatistics"/>
+        /// gathered from the parallel‑operations benchmark.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="pipeline"/> is <c>null</c>.</exception>
         public static async Task<PipelineStatistics> RunParallelOperationsAndCollectStatistics(this ResiliencePipelineBenchmarks pipeline)
         {
             ArgumentNullException.ThrowIfNull(pipeline);
