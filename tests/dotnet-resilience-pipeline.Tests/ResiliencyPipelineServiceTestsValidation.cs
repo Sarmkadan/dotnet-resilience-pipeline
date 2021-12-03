652a7e95461eaf3658ef6b69e4b68eb5c031d7c6
Vladyslav Zaiets <rutova2@gmail.com>
2021-12-03 15:00:47 +0200

    Defensive checks around ResiliencyPipelineService
    found this while looking at something else.

--- tests/dotnet-resilience-pipeline.Tests/ResiliencyPipelineServiceTestsValidation.cs
+++ tests/dotnet-resilience-pipeline.Tests/ResiliencyPipelineServiceTestsValidation.cs
@@ -1,4 +1,5 @@
 #nullable enable
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,19 +7,22 @@ using System.Linq;
 namespace DotNetResiliencePipeline.Tests
 {
     /// <summary>
-    /// Validation helpers for <see cref="ResiliencyPipelineServiceTests"/>.
+    /// Provides validation extension methods for <see cref="ResiliencyPipelineServiceTests"/> instances.
     /// </summary>
     public static class ResiliencyPipelineServiceTestsValidation
     {
         /// <summary>
         /// Validates the public members of a <see cref="ResiliencyPipelineServiceTests"/> instance.
-        /// Since the test class only contains methods (no state), this validation currently
+        /// Since the test class only contains test methods and no state, this validation
         /// always returns an empty list, indicating no problems.
         /// </summary>
-        /// <param name="value">The test instance to validate.</param>
-        /// <returns>A read‑only list of human‑readable validation problems.</returns>
+        /// <param name="value">The test instance to validate. Cannot be <see langword="null"/>.</param>
+        /// <returns>A read-only list of human-readable validation problems.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
         public static IReadOnlyList<string> Validate(this ResiliencyPipelineServiceTests value)
         {
+            ArgumentNullException.ThrowIfNull(value);
+
             // The test class does not expose any data members that can be invalid.
             // Returning an empty list signals that the instance is considered valid.
             return Array.Empty<string>();
@@ -27,10 +31,12 @@ namespace DotNetResiliencePipeline.Tests
         /// <summary>
         /// Determines whether the supplied <see cref="ResiliencyPipelineServiceTests"/> instance is valid.
         /// </summary>
-        /// <param name="value">The test instance to check.</param>
+        /// <param name="value">The test instance to check. Cannot be <see langword="null"/>.</param>
         /// <returns><c>true</c> if no validation problems were found; otherwise <c>false</c>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
         public static bool IsValid(this ResiliencyPipelineServiceTests value)
         {
+            ArgumentNullException.ThrowIfNull(value);
             return !value.Validate().Any();
         }
 
@@ -38,10 +44,13 @@ namespace DotNetResiliencePipeline.Tests
         /// Ensures that the supplied <see cref="ResiliencyPipelineServiceTests"/> instance is valid.
         /// Throws an <see cref="ArgumentException"/> if any validation problems are detected.
         /// </summary>
-        /// <param name="value">The test instance to validate.</param>
+        /// <param name="value">The test instance to validate. Cannot be <see langword="null"/>.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
         /// <exception cref="ArgumentException">Thrown when validation problems are present.</exception>
         public static void EnsureValid(this ResiliencyPipelineServiceTests value)
         {
+            ArgumentNullException.ThrowIfNull(value);
+
             var problems = value.Validate();
             if (problems.Any())
             {
