3680810da0663b3d92ecd67dc7f4ca2fda099c8a
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-08 12:13:43 +0000

    chore: add file header and #nullable enable to ITimeoutStrategy.cs

--- src/Domain/Policies/ITimeoutStrategy.cs
+++ src/Domain/Policies/ITimeoutStrategy.cs
@@ -1,4 +1,8 @@
 #nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
 
 namespace DotNetResiliencePipeline.Domain.Policies;
 
